3444e72b114325bc7775421a76e6d5d54973268a
DiegoCoura <diegogomes.10.9@gmail.com>
2024-02-02 17:21:15 -0300

    feat: html structure

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
-import _ from "lodash";
 import "./style.css";
 
-function component() {
-  const element = document.createElement("div");
 
-  element.innerHTML = "Hello World"
-
-  return element;
-}
-
-document.body.appendChild(component());
 
